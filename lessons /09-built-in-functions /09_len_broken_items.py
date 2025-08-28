f842f7bbdec3b738c9576f476cb2d2f44f2155ad
AbqoorKids <abqoor.kids@gmail.com>
2025-08-28 19:19:36 +0400

    Create 09_len_broken_items.py

---
+++ lessons /09-built-in-functions /09_len_broken_items.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# 09_len_broken_items.py
+# Using len() to find the number of items in a list
+
+# This list contains items that need fixing
+broken_items = ["chair", "table", "bed", "desk"]
+
+# Print the number of items
+print("Number of items that need fixing:", len(broken_items))
