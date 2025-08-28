7d249cc187950953874f2a4c1e04802818630860
AbqoorKids <abqoor.kids@gmail.com>
2025-08-28 19:20:56 +0400

    Create 09_sorted_screws.py

---
+++ lessons /09-built-in-functions /09_sorted_screws.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# 09_sorted_screws.py
+# Using sorted() to sort a list of numbers
+
+# List of screws
+screws = [1, 7, 5, 4, 2, 3, 6, 8]
+
+# Sort the screws
+sorted_screws = sorted(screws)
+
+print("Screws after sorting:", sorted_screws)
