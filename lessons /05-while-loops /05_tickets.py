fc3d9273cbb9c98875356d9e2cd54566c4912090
AbqoorKids <abqoor.kids@gmail.com>
2025-08-28 15:43:21 +0400

    Create 05_tickets.py

---
+++ lessons /05-while-loops /05_tickets.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# 05_tickets.py
+# Example: Codey rides according to the number of tickets
+
+tickets = 4
+
+while tickets > 0:
+    print("🎢 Codey is playing on the train — Tickets left:", tickets)
+    tickets = tickets - 1  # or we can use tickets -= 1
+
+print("✅ Tickets are over — The game is finished")
