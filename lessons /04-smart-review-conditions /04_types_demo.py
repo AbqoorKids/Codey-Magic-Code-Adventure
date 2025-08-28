26dde3361db450651892dcb6c131a75eeb68fd9b
AbqoorKids <abqoor.kids@gmail.com>
2025-08-28 15:30:26 +0400

    Create 04_types_demo.py

---
+++ lessons /04-smart-review-conditions /04_types_demo.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# 04_types_demo.py
+# Example: Identifying the data type
+
+print(type(False))   # Boolean type
+print(type("Banana"))   # String type
+print(type(200))     # Integer type
