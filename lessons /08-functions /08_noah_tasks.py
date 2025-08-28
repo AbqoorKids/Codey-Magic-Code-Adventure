b7c2e9fe7a5b69e6cd97f38b91b461686a4cad70
AbqoorKids <abqoor.kids@gmail.com>
2025-08-28 19:07:51 +0400

    Create 08_noah_tasks.py

---
+++ lessons /08-functions /08_noah_tasks.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# 08_noah_tasks.py
+# Function named noah_tasks containing Noah's tasks
+
+def noah_tasks():
+    print("Organize the stuff")
+    print("Take out the trash")
+
+# Call the function
+noah_tasks()
