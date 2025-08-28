da6c8be18285fab474580018a2b7333af4da492a
AbqoorKids <abqoor.kids@gmail.com>
2025-08-28 19:01:18 +0400

    Create 08_clean_room.py

---
+++ lessons /08-functions /08_clean_room.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# 08_clean_room.py
+# Function named clean_room containing 3 tasks
+
+def clean_room():
+    # Inside it we put three tasks    
+    print("🧺 Collect the items")
+    print("🧽 Mop the floor")
+    print("🗂️ Organize the stuff")
+
+# Call the function to execute it
+clean_room()
