a10c47ee12d1d67d5f6c25166da1a69099b73da5
AbqoorKids <abqoor.kids@gmail.com>
2025-08-28 15:24:00 +0400

    Create 04_max_number.py

---
+++ lessons /04-smart-review-conditions /04_max_number.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# 04_max_number.py
+# Example: Finding the largest number among three numbers
+
+a = 22
+b = 37
+c = 5
+
+if a > b and a > c:
+    print(a)
+elif b > a and b > c:
+    print(b)
+else:
+    print(c)
