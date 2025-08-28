a43a03cf5be472d0bf869efd66f1a7a5aac3dd4a
AbqoorKids <abqoor.kids@gmail.com>
2025-08-28 14:57:52 +0400

    Create 02_crowns.py

---
+++ lessons /02-inputs-types /02_crowns.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# 02_crowns.py
+# Example: Check if the number of crowns is even or odd
+
+crowns = int(input("How many crowns does the lion have? "))
+if crowns % 2 == 0:
+    print("The number of crowns is even")
+else:
+    print("The number of crowns is odd")
