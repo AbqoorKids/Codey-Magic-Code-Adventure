60b386f3d0231d6ed7e5f9ae8057960c9b0216b6
AbqoorKids <abqoor.kids@gmail.com>
2025-08-28 15:56:56 +0400

    Create 06_friends_list.py

---
+++ lessons /06-for-loops-lists /06_friends_list.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# 06_friends_list.py
+# Printing friends' names
+
+my_friends = ["Luna", "Max", "Zara"]
+
+# Loop that goes through each item in the list
+for friend in my_friends:
+    # Prints each friend's name
+    print(friend)
