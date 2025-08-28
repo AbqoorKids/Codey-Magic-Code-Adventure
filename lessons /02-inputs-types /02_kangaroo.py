c8d3824be1c434d3d1517ddb1a3a0c81a05a1377
AbqoorKids <abqoor.kids@gmail.com>
2025-08-28 15:00:51 +0400

    Create 02_kangaroo.py

---
+++ lessons /02-inputs-types /02_kangaroo.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# 02_kangaroo.py
+# Example: Decide if the kangaroo is a baby based on age
+
+age = int(input("How old is the kangaroo? "))
+if age < 10:
+    print("The kangaroo is a baby")
+else:
+    print("The kangaroo is not a baby")
