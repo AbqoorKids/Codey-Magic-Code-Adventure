9e277568837cbdcc0b40e326ca9126d4a159bc21
AbqoorKids <abqoor.kids@gmail.com>
2025-08-28 15:58:31 +0400

    Create 06_seafood_tasting.py

---
+++ lessons /06-for-loops-lists /06_seafood_tasting.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# 06_seafood_tasting.py
+# Iterating over a list of items and printing each one
+
+sea_food = ["Fish", "Lobster", "Shrimp", "Oyster", "Crab"]
+
+for dish in sea_food:
+    print("🍽️ Codey tastes:", dish)
